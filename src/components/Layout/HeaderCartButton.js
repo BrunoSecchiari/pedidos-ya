4daff5b23583c40d39ecce44ad8c873542f4706d
Bruno Secchiari <bsecchia@ar.ibm.com>
2022-09-23 21:16:29 -0300

    Remove Item From Cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-content";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-  const ctx = useContext(CartContext);
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
 
-  const numberOfCartItems = ctx.items.reduce((currentNumber, item) => {
+  const ctx = useContext(CartContext);
+  const { items } = ctx;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const buttonStyle = `${styles.button} ${
+    buttonIsHighlighted ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonStyle} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
